f7d5bf6ce5ccdd1d389b76006c1ed827f58b658e
Luis-ro-cki <l29472954@gmail.com>
2026-05-25 17:58:06 -0600

    Crear config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    prefix: '.',
+    owner: ['521XXXXXXXXX'], // Pon tu número sin +
+    apis: {
+        // Crea cuenta gratis en cada una y pega tu key
+        neoxr: 'TU_KEY_NEOXR', // https://neoxr-api.com
+        lolhuman: 'TU_KEY_LOLHUMAN', // https://api.lolhuman.xyz
+        zenz: 'TU_KEY_ZENZ' // https://zenzapis.xyz
+    }
+}
